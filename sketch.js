93814ea5f56d29062aa946f8abca192db9b5f035
coder2110 <140773879+coder2110@users.noreply.github.com>
2024-01-21 11:50:04 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+//Project 53 Questionnare
+//
+// Question 1
+//  3
+//
+// Question 2
+//  1
+//
+// Question 3 
+// It divides the code into different sections and makes it more organized
+//
+// Question 4
+// Relative positioning is placing an element relative to where it will be normally placed
+// Absolute positioning is placing an element closest to the element before it
+//
+// Question 5
+// Opacity is and element used on objects that determines is transparency, though it is the opposite of transparency
+// 
+// Question 6
+// React Native is a combination of JavaScript, HTML, and CSS
+//
+// Question 7
+// Expo
+//
+// Question 8
+// First you down load the app and link it to your device, and then every time I open the app it says Device connected!
+//
+// Question 9
+// The render function is used to display the components on screen
+//
+// Question 10
+// The return function is used to return a value from a function
+//
+// Question 11
+// We have the button, the header, and the sound when the button is pressed
+//
+//
+//
